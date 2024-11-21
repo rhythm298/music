ed315d705eb10b73eadb2970da6b4bf8674b7864
rhythm298 <rhythmmehta29@gmail.com>
2024-11-21 16:16:26 +0530

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,1 +1,41 @@
+require("dotenv").config();
 
+module.exports = {
+    token: process.env.TOKEN || "",  // your bot token
+    clientID: process.env.CLIENT_ID || "", // your bot client id
+    prefix: process.env.PREFIX || "!", // bot prefix
+    ownerID: process.env.OWNERID || "", //your discord id
+    SpotifyID: process.env.SPOTIFYID || "",
+    SpotifySecret: process.env.SPOTIFYSECRET || "",
+    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    embedColor: process.env.COlOR || 0x303236, // embed colour
+    logs: process.env.LOGS || "", // channel id for guild create and delete logs
+    links: {
+        img: process.env.IMG || 'https://cdn.discordapp.com/icons/905075946426613760/b4d47f7be31abeb7bb5c871a13de8610.webp', //setup system background image 
+        support: process.env.SUPPORT || 'https://discord.gg/9zzcvRqb3A', //support server invite link
+        invite: process.env.INVITE || 'https://discord.com/api/oauth2/authorize?client_id=1027127109598060604&permissions=8&scope=bot%20applications.commands' //bot invite link
+    },
+    nodes: [
+        {
+            host: process.env.NODE_HOST || "lava.link",
+            port: parseInt(process.env.NODE_PORT || "80"),
+            password: process.env.NODE_PASSWORD || "NotJust",
+            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+
+        }
+    ],
+
+}
+
+function parseBoolean(value) {
+    if (typeof (value) === 'string') {
+        value = value.trim().toLowerCase();
+    }
+    switch (value) {
+        case true:
+        case "true":
+            return true;
+        default:
+            return false;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const MusicBot = require("./structures/Client");
+const client = new MusicBot();
+
+client.connect()
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log(reason, p);
+});
+
+process.on('uncaughtException', (err, origin) => {
+    console.log(err, origin);
+});
+
+process.on('uncaughtExceptionMonitor', (err, origin) => {
+    console.log(err, origin);
+});
+
+
+module.exports = client;
